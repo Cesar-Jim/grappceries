3800192014363526824ebac6a3952f60738936bf
Cesar Jimenez <cijgr@hotmail.com>
2019-03-31 21:48:52 -0600

    USer authentication completed

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -7,6 +7,7 @@ const expressValidator = require("express-validator");
 
 const session = require("express-session");
 const flash = require("express-flash");
+const passportConfig = require("./passport-config");
 
 module.exports = {
   init(app, express) {
@@ -19,10 +20,16 @@ module.exports = {
         secret: process.env.cookieSecret,
         resave: false,
         saveUninitialized: false,
-        cookie: { maxAge: 60000 }
+        cookie: { maxAge: 1.21e9 }
       })
     );
     app.use(flash());
+    passportConfig.init(app);
+
+    app.use((req, res, next) => {
+      res.locals.currentUser = req.user;
+      next();
+    });
     app.use(express.static(path.join(__dirname, "..", "assets")));
   }
 };
